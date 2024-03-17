2e4e02ea6a6ba670baf36150ebee298ebbfdce21
Shanel-A <73032884+Shanel-A@users.noreply.github.com>
2024-03-18 01:48:19 +0800

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,7 @@
 $(document).ready(function(){
 	"use strict";
-    
-        /*==================================
-* Author        : "ThemeSine"
-* Template Name : Khanas HTML Template
-* Version       : 1.0
-==================================== */
 
-
-
-/*=========== TABLE OF CONTENTS ===========
-1. Scroll To Top 
-2. Smooth Scroll spy
-3. Progress-bar
-4. owl carousel
-5. welcome animation support
-======================================*/
-
-    // 1. Scroll To Top 
+    //Scroll To Top 
 		$(window).on('scroll',function () {
 			if ($(this).scrollTop() > 600) {
 				$('.return-to-top').fadeIn();
@@ -34,13 +18,12 @@ $(document).ready(function(){
 	
 	
 	
-	// 2. Smooth Scroll spy
+	//Smooth Scroll
 		
 		$('.header-area').sticky({
            topSpacing:0
         });
 		
-		//=============
 
 		$('li.smooth-menu a').bind("click", function(event) {
 			event.preventDefault();
@@ -55,7 +38,7 @@ $(document).ready(function(){
 			offset:0
 		});
 
-	// 3. Progress-bar
+	//Progress-bar
 	
 		var dataToggleTooTip = $('[data-toggle="tooltip"]');
 		var progressBar = $(".progress-bar");
@@ -71,47 +54,9 @@ $(document).ready(function(){
 			});
 		}
 	
-	// 4. owl carousel
-	
-		// i. client (carousel)
-		
-			$('#client').owlCarousel({
-				items:7,
-				loop:true,
-				smartSpeed: 1000,
-				autoplay:true,
-				dots:false,
-				autoplayHoverPause:true,
-				responsive:{
-						0:{
-							items:2
-						},
-						415:{
-							items:2
-						},
-						600:{
-							items:4
-
-						},
-						1199:{
-							items:4
-						},
-						1200:{
-							items:7
-						}
-					}
-				});
-				
-				
-				$('.play').on('click',function(){
-					owl.trigger('play.owl.autoplay',[1000])
-				})
-				$('.stop').on('click',function(){
-					owl.trigger('stop.owl.autoplay')
-				})
 
 
-    // 5. welcome animation support
+    //Welcome animation 
 
         $(window).load(function(){
         	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
